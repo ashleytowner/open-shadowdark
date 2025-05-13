e32c8a2561eb04dd0a07f2d0e75d7f6dba6ae574
Ashley Towner <git@ashtowner.com>
2025-05-13 19:51:56 +0930

    feat: Serve static files

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const path = require('path');
 app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
 	const body = `
