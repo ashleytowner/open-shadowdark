bf5f4f78c492f2a9f735c66ae793ac98b09ab818
Ashley Towner <git@ashtowner.com>
2026-05-24 18:54:43 +0930

    feat: Switched adapter from vercel to node

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,29 +5,31 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
 
-import vercel from "@astrojs/vercel";
+import node from "@astrojs/node";
 
 const site = "https://openshadowdark.com";
 
 // https://astro.build/config
 export default defineConfig({
-	site,
-	integrations: [mdx(), sitemap()],
+  site,
+  integrations: [mdx(), sitemap()],
 
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
 
-	fonts: [
-		{
-			name: "Montserrat",
-			cssVariable: "--font-montserrat",
-			provider: fontProviders.fontsource(),
-			weights: [400, 700], // Example weights
-			styles: ["normal", "italic"],
-			fallbacks: ["sans-serif"],
-		},
+  fonts: [
+      {
+          name: "Montserrat",
+          cssVariable: "--font-montserrat",
+          provider: fontProviders.fontsource(),
+          weights: [400, 700], // Example weights
+          styles: ["normal", "italic"],
+          fallbacks: ["sans-serif"],
+      },
 	],
 
-	adapter: vercel(),
+  adapter: node({
+    mode: "standalone"
+  })
 });
